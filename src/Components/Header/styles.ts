c8bf991af4f36bcbe99a6de16644d3d91db9519b
José Jorge <jorgesouza621@gmail.com>
2024-08-05 20:52:11 -0300

    Alteração na estilização do Header

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -2,9 +2,16 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
   height: 18.5rem;
-  width: 100%;
   background-image: url('/src/assets/bg-header.svg');
   background-size: cover;
-  background-repeat: no-repeat;
   background-position: center;
+
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 9.35rem;
+    height: 6.125rem;
+    margin-top: 4rem;
+  }
 `
