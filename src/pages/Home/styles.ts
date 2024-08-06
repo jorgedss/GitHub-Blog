a0830c6741a53d5e1cf17234222f21bb90d33b69
José Jorge <jorgesouza621@gmail.com>
2024-08-05 21:09:36 -0300

    Responsividade na Home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 54rem;
+  max-width: 56rem;
   width: 100%;
+
   margin: -6.625rem auto 2rem;
+  padding: 0 2rem;
 
   main {
     margin: 0 auto;
@@ -13,5 +15,10 @@ export const HomeContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
     gap: 2rem;
+  }
+  @media only screen and (max-width: 699px) {
+    main {
+      grid-template-columns: 1fr;
+    }
   }
 `
