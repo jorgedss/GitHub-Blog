8aa11f066a092a0e7120d1ce663067f0d499b324
Jorge Souza <121060792+jorgedss@users.noreply.github.com>
2024-08-08 16:05:43 -0300

    Solucionando erro do styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { StyleSheetManager } from 'styled-components'
+import emotionIsPropValid from '@emotion/is-prop-valid'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StyleSheetManager shouldForwardProp={emotionIsPropValid}>
+    <App />
+  </StyleSheetManager>,
+)
